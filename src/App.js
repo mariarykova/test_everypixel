f153c0cfe02e66e76b0c3261f015cd7ac3e0b2b0
Maria Rykova <maria.nigay@yandex.ru>
2020-08-28 22:50:26 +0500

    styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React,  { useState, useEffect  } from 'react';
 import './App.css';
 import Data from './data.json';
-import Checkbox from '@material-ui/core/Checkbox';
 import Footer from './components/Footer/Footer.js';
+
+import Checkbox from '@material-ui/core/Checkbox';
 import DeleteIcon from '@material-ui/icons/Delete';
 import GetAppIcon from '@material-ui/icons/GetApp';
 import CreditCardIcon from '@material-ui/icons/CreditCard';
 import ShareIcon from '@material-ui/icons/Share';
 import VisibilityIcon from '@material-ui/icons/Visibility';
-import Pagination from '@material-ui/lab/Pagination';
 
 
 class App extends React.Component {
@@ -23,92 +23,91 @@ class App extends React.Component {
       }),
       currentPage: 1,
       imagesPerPage: 6,
-      activePage: '',
       selectAllChecked: false
     };
+
     this.handleClick = this.handleClick.bind(this);
     this.selectedImages = this.selectedImages.bind(this);
     this.deleteSelectedImages = this.deleteSelectedImages.bind(this);
     this.selectAllImages = this.selectAllImages.bind(this);
+
   }
 
-  
+// Logic for clicking on page number
+  handleClick(event) {
+    this.setState({
+     currentPage: Number(event.target.id)
+    });
+  }
 
-      handleClick(e, index) {
-        this.setState({
-          currentPage: Number(index),
-          activePage: index
-        });
-      }
-
-      selectedImages(indexSelected) {
-        const nextImagesState = this.state.images.map((image, index) => {
-        let nextSelectedState = image.isSelected;
-          if (index === indexSelected) {
-          nextSelectedState = !image.isSelected;
-        }
-          return {
-            ...image,
-            isSelected: nextSelectedState
-          };
-        });
+// Logic for selecting one image
+  selectedImages(indexSelected) {
+    const nextImagesState = this.state.images.map((image, index) => {
+    let nextSelectedState = image.isSelected;
+    if (index === indexSelected) {
+      nextSelectedState = !image.isSelected;
+    }
+    return {
+      ...image,
+      isSelected: nextSelectedState
+    };
+    });
         
-        this.setState({
-        ...this.state,
-        images: nextImagesState,
-        });
+    this.setState({
+      ...this.state,
+      images: nextImagesState,
+      });
+    }
+
+// Logic for selecting all images
+  selectAllImages(){
+    let selectAllChecked = !this.state.selectAllChecked;
+    this.setState({
+      selectAllChecked: selectAllChecked
+     });
+
+    let images = this.state.images.slice();
+    if(selectAllChecked){
+      for(var i = 0; i < this.state.images.length; i++)
+        images[i].isSelected = true;
+      } else {
+        for(var i = 0; i < this.state.images.length; i++)
+        images[i].isSelected = false;
       }
 
-     selectAllImages(){
-        let selectAllChecked = !this.state.selectAllChecked;
-          this.setState({
-              selectAllChecked: selectAllChecked
-          });
+      this.setState({
+      images: images
+     });
+  }
 
-          let images = this.state.images.slice();
-          if(selectAllChecked){
-              for(var i = 0; i < this.state.images.length; i++)
-                  images[i].isSelected = true;
-          }
-          else {
-              for(var i = 0; i < this.state.images.length; i++)
-                  images[i].isSelected = false;
+// Logic for deleting one image
+  deleteImage(indexSelected) {
+    const newImages = this.state.images.filter((image, index) => indexSelected !== index)
+    this.setState({
+      ...this.state,
+      images: newImages
+    });
+  }
+// Logic for deleting all selected images
+  deleteSelectedImages() {
+    const newImages = this.state.images.filter(image => image.isSelected === false)
+    this.setState ({
+     ...this.state,
+     images: newImages
+    });
+  }
 
-          }
-          this.setState({
-              images: images
-          });
-     }
-
-
-      deleteImage(indexSelected) {
-       const newImages = this.state.images.filter((image, index) => indexSelected !== index)
-        this.setState({
-          ...this.state,
-          images: newImages
-       });
-      }
-      
-      deleteSelectedImages() {
-        const newImages = this.state.images.filter(image => image.isSelected === false)
-        this.setState ({
-          ...this.state,
-          images: newImages
-        });
-      }
-
-  
-   render() {
+  render() {
     const { images, currentPage, imagesPerPage } = this.state;
     
   // Logic for displaying images
-
     const indexOfLastImage = currentPage * imagesPerPage;
     const indexOfFirstImage = indexOfLastImage - imagesPerPage;
     const currentImages = images.slice(indexOfFirstImage, indexOfLastImage);
 
     const renderImages = currentImages.map((image, index) => {
 
+  // Logic for adding an active class to make visible a checkbox icon
       const { isSelected } = this.state.images;
       let classNames = 'checkbox';
       let  visibility = 'visibility';
@@ -117,76 +116,62 @@ class App extends React.Component {
       visibility += ' active';
     }
 
-      return (
-        <div key={index} className="images">
-            <div className="icons">
-                <div className={classNames}>
-                  <Checkbox 
-                        checked={image.isSelected}
-                        onChange={e => {
-                          this.selectedImages(indexOfFirstImage + index);}
-                        } 
-                  />
-                </div>
-                <div className="icons__left">
-                  <div className="share"><ShareIcon /></div>
-                  <div className="trash"><DeleteIcon onClick={e => {this.deleteImage(indexOfFirstImage + index);}} /></div>
-                  <div className="download"><GetAppIcon /></div>
-                </div>
-            </div> 
-            <div className={visibility}><VisibilityIcon fontSize="large" /></div>         
-            <img src={image.sample_url} alt="Image" className="image" />
-            <div className="title">
-              <div className="title__info">
-                <span className="question-box">?</span>
-                <span className="triangle">&#9660;</span>
-                <span className="text">Выберите лицензию</span>
-              </div>
-              <div><CreditCardIcon className="creditcard"/></div>
-            </div>
+    return (
+      <div key={index} className="images">
+        <div className="icons">
+          <div className={classNames}>
+            <Checkbox 
+              checked={image.isSelected}
+              onChange={e => {
+              this.selectedImages(indexOfFirstImage + index);}
+              } 
+            />          
+          </div>
+          <div className="icons__left">
+            <div className="share" title="Поделиться"><ShareIcon /></div>
+            <div className="trash" title="Удалить"><DeleteIcon onClick={e => {this.deleteImage(indexOfFirstImage + index);}} /></div>
+            <div className="download" title="Скачать"><GetAppIcon /></div>
+          </div>
+        </div> 
+        <div className={visibility}><VisibilityIcon fontSize="large" /></div>         
+        <img src={image.sample_url} alt="Image" className="image" />
+        <div className="title">
+          <div className="title__info">
+            <span className="question-box">?</span>
+            <span className="triangle">&#9660;</span>
+            <span className="text">Выберите лицензию</span>
+          </div>
+          <div><CreditCardIcon className="creditcard"/></div>
         </div>
-      )
-    });
+      </div>
+      )});
 
     // Logic for displaying page numbers
     const pageNumbers = [];
-    for (let i = 1; i <= Math.ceil(images.length / imagesPerPage); i++)
-       pageNumbers.push(i)
+      for (let i = 1; i <= Math.ceil(images.length / imagesPerPage); i++)
+      pageNumbers.push(i)
 
-    const renderPageNumbers = pageNumbers.map((number, index) => {
-      const active = this.state.activePage;
-      let classPag = 'pagination';
-      if (active === number) {
-        classPag += ' activepag';
-      }
-      
+    const renderPageNumbers = pageNumbers.map(number => {
       return (
-        <div className={classPag}
+        <div
           key={number}
           id={number}
-          onClick={(e) => this.handleClick(e, number)}
-          >
+          className={ number === this.state.currentPage ? 'pagination activepage': 'pagination'}
+          onClick={this.handleClick}>
           {number}
         </div>       
       );
     });
 
-
-        return (
+    return (
       <div className="App">
         <div className="header">{this.state.images.length} ИЗОБРАЖЕНИЙ</div>
-        <div className="wrapper">
-          {renderImages}
-        </div>
-        <div className="pages">
-          {renderPageNumbers}
-        </div>
-          <Footer images={images} 
-          selectAllImages={this.selectAllImages}
-          deleteSelectedImages={this.deleteSelectedImages}
-         />
+        <div className="wrapper">{renderImages}</div>
+        <div className="pages">{renderPageNumbers}</div>
+        <Footer images={images} selectAllImages={this.selectAllImages} deleteSelectedImages={this.deleteSelectedImages} />
       </div>
     );
   }
 }
+
 export default App;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,32 +1,35 @@
 import React, { useState, Component } from 'react';
-import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
 import './Footer.css';
+
+import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
 import CreditCardIcon from '@material-ui/icons/CreditCard';
 import DeleteIcon from '@material-ui/icons/Delete';
 import AssignmentReturnedIcon from '@material-ui/icons/AssignmentReturned';
 import AddBoxIcon from '@material-ui/icons/AddBox';
 
 const Footer = ({ images, deleteSelectedImages, selectAllImages }) => {
+
+// Logic for counting selected images
   let selected = 0;
-        for(let i = 0; i < images.length; i++)
-            if(images[i].isSelected == true)
-              selected++;
+  for(let i = 0; i < images.length; i++)
+  if(images[i].isSelected == true)
+    selected++;
   
-     return (
-      <div className="footer">
-        <div className="left">
-          <button className="button"><RemoveCircleOutlineIcon onClick={selectAllImages} /></button>
-          <div><span className="selected">{selected} </span></div>
-        </div>
-        <div className="center">
-           <button className="button"><DeleteIcon onClick={deleteSelectedImages} /></button>
-          <div><AssignmentReturnedIcon /></div>
-          <div><CreditCardIcon /></div>
-          <div><AddBoxIcon /></div>
-        </div>        
-        <div className="right">Для отмены нажмите ESC</div>
+  return (
+    <div className="footer">
+      <div className="left">
+        <button className="button button__selecte" title="Выбрать все картинки"><RemoveCircleOutlineIcon onClick={selectAllImages} /></button>
+        <div><span className="selected">{selected}</span>выбрано изображений</div>
       </div>
-    );        
-}
+      <div className="center">
+        <button className="button" title="Удалить"><DeleteIcon onClick={deleteSelectedImages} /></button>
+        <div><AssignmentReturnedIcon /></div>
+        <div><CreditCardIcon /></div>
+        <div><AddBoxIcon /></div>
+      </div>        
+      <div className="right">Для отмены нажмите ESC</div>
+    </div>
+  );        
+  }
 
 export default Footer;
